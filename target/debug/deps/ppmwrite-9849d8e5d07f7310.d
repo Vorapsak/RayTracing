af2b1e95519e0c6e786fbff227898499c0263398
Jeff L'Heureux <chessRPG@hotmail.com>
2018-07-24 10:56:28 -0700

    use cargo and hopefully ignore /target/

---
+++ target/debug/deps/ppmwrite-9849d8e5d07f7310.d
@@ -0,0 +1,5 @@
+/home/vorap/cproj/raytracing1/target/debug/deps/ppmwrite-9849d8e5d07f7310: src/bin/ppmwrite.rs
+
+/home/vorap/cproj/raytracing1/target/debug/deps/ppmwrite-9849d8e5d07f7310.d: src/bin/ppmwrite.rs
+
+src/bin/ppmwrite.rs:
---
+++ target/debug/ppmwrite.d
@@ -0,0 +1,1 @@
+/home/vorap/cproj/raytracing1/target/debug/ppmwrite: /home/vorap/cproj/raytracing1/src/bin/ppmwrite.rs
