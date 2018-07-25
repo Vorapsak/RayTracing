c6fa48aeba349e1585afbbd73ba811b209392f90
Jeff L'Heureux <chessRPG@hotmail.com>
2018-07-25 13:42:56 -0700

    it works now

--- src/ppmwritevec3.rs
+++ src/ppmwritevec3.rs
@@ -1,5 +1,6 @@
-//use vector3::Vector3;
-use test;
+mod vector3;
+use vector3::Vector3;
+mod test;
 
 fn main() {
     let nx = 200;
@@ -8,13 +9,11 @@ fn main() {
 
 	for j in (0..ny).rev() {
 		for i in 0..nx{
-//			let color: vector3 = vector3::new( 
-//			(i as f64) / (nx as f64) * 255.99, 
-//			(j as f64) / (ny as f64) * 255.99, 
-//			0.2 * 255.99 );
-//			let color = Vector3::new(0.0, 0.0, 1.0);
-//			println!("{} {} {}", color.x as i32, color.y as i32, color.z as i32);
-			println!("{}", hi());
+			let color = Vector3::new( 
+			(i as f64) / (nx as f64) * 255.99, 
+			(j as f64) / (ny as f64) * 255.99, 
+			0.2 * 255.99 );
+			println!("{} {} {}", color.x as i32, color.y as i32, color.z as i32);
 		}
 	}
 }
